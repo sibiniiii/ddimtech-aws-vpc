2743dc95782849f72c3f86dbdf87a4b9513fb0c3
sibiniiii <101381931+sibiniiii@users.noreply.github.com>
2024-06-17 17:17:31 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "sb_main_vpc" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+
+  tags = {
+    Name = var.vpc_tag,
+    tag = var.tags
+  }
+}
